00a0343f177d7aaead2949ddf32a0c52ec85bd4d
fattobi <fattobi>
2004-11-17 14:50:11 +0000

    config.xml and db-files are now copied to user-homedir

--- liabolo/src/org/liabolo/common/Configurator.java
+++ liabolo/src/org/liabolo/common/Configurator.java
@@ -1,8 +1,8 @@
 package org.liabolo.common;
 
 
-import java.io.File;
-import org.jconfig.*; 
+import java.io.*;
+import org.jconfig.*;
 import org.jconfig.handler.*;
 
 
@@ -20,6 +20,8 @@ public class Configurator {
 
     public static String configMainDir = "../config";
     public static String logMainDir = "../log";
+    public static String userDir = System.getProperty("user.home");
+    public static File liaboloDir;
 	
 
     public static void config() {
@@ -31,15 +33,36 @@ public class Configurator {
             if(System.getProperty("exist.home")==null)
                 System.setProperty("exist.home","../exist");
               
-            File file = new File(configMainDir+"/config.xml");
+            File configFile = new File(configMainDir+"/config.xml");
 			handler = new XMLFileHandler();
-			handler.setFile(file);
+			handler.setFile(configFile);
 			try {
 			cm.load(handler,"liaboloConfig");
 			config = cm.getConfiguration("liaboloConfig");
 			} catch (Exception e){
 				System.err.println(e);
 			}
+			
+			// check, if a liabolo-dir exist in user-dir. If not, create it and
+			// copy user-specific files
+			liaboloDir = new File(userDir, ".liabolo");
+			if (!liaboloDir.exists()) {
+				System.out.println("Liabolo-Directory not found! Creating it...");
+				liaboloDir.mkdir();
+			} else System.out.println("Liabolo-Directory exists!");
+			System.out.println("Copy files...");
+			fileCopy(configFile, "config.xml");
+			// copy eXist files
+			File collections = new File("exist/data","collections.dbx");
+			File dom = new File("exist/data","dom.dbx");
+			File elements = new File("exist/data","elements.dbx");
+			File words = new File("exist/data","words.dbx");
+			
+			fileCopy(collections,"collections.dbx");
+			fileCopy(dom,"dom.dbx");
+			fileCopy(elements,"elements.dbx");
+			fileCopy(words,"words.dbx");
+			
 
 
             configured = true;
@@ -87,6 +110,46 @@ public class Configurator {
             return false;
         }
     }
+    
+    private static void fileCopy(File source, String d){
+
+    	File dest = new File(liaboloDir, d);
+    	if (dest.exists()) return; // no need to copy file
+    	
+    	FileInputStream fis;
+    	BufferedInputStream bis;
+    	FileOutputStream fos;
+    	BufferedOutputStream bos;
+    	byte[] b;
+    	
+    	
+    	try { // übergebene Dateien öffnen
+    		fis = new FileInputStream(source);
+    		fos = new FileOutputStream(dest);
+    	} catch (FileNotFoundException ex) {
+    		throw new Error("File not found");
+    	}
+    	
+    	// Dateien umlenken auf Puffer
+    	bis = new BufferedInputStream(fis);
+    	bos = new BufferedOutputStream(fos);
+    	int read;
+    	try { // Datei lesen, schreiben und schließen
+    		b = new byte[bis.available()];
+    		while (true){
+    			read = bis.read(b);
+    			if (read == -1) break;
+    			bos.write(b);
+    		
+    		}
+    		bis.close();
+    		bos.close();
+    	} catch (IOException e) {
+    		System.out.println("Files could not be copied!");
+    	}
+    } 
+
+    	
 
 
 }
